5b001295b073da97372c88290dbda3157425deca
mekya <ahmetmermerkaya@gmail.com>
2020-02-29 20:34:43 +0300

    Support multi-track

---
+++ webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/ConferenceActivity.java
@@ -0,0 +1,153 @@
+package io.antmedia.webrtc_android_sample_app;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.webrtc.SurfaceViewRenderer;
+
+import java.util.ArrayList;
+
+import de.tavendo.autobahn.WebSocket;
+import io.antmedia.webrtcandroidframework.ConferenceManager;
+import io.antmedia.webrtcandroidframework.IWebRTCListener;
+import io.antmedia.webrtcandroidframework.apprtc.CallActivity;
+import io.antmedia.webrtcandroidframework.apprtc.CallFragment;
+
+import static io.antmedia.webrtcandroidframework.apprtc.CallActivity.EXTRA_CAPTURETOTEXTURE_ENABLED;
+
+public class ConferenceActivity extends Activity implements IWebRTCListener {
+
+
+    private ConferenceManager conferenceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set window styles for fullscreen-window size. Needs to be done before
+        // adding content.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        //getWindow().getDecorView().setSystemUiVisibility(getSystemUiVisibility());
+
+        setContentView(R.layout.activity_conference);
+
+        SurfaceViewRenderer publishViewRenderer = findViewById(R.id.publish_view_renderer);
+        ArrayList<SurfaceViewRenderer> playViewRenderers = new ArrayList<>();
+
+        playViewRenderers.add(findViewById(R.id.play_view_renderer1));
+        playViewRenderers.add(findViewById(R.id.play_view_renderer2));
+        playViewRenderers.add(findViewById(R.id.play_view_renderer3));
+        playViewRenderers.add(findViewById(R.id.play_view_renderer4));
+
+        // Check for mandatory permissions.
+        for (String permission : CallActivity.MANDATORY_PERMISSIONS) {
+            if (this.checkCallingOrSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission " + permission + " is not granted", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+
+        this.getIntent().putExtra(EXTRA_CAPTURETOTEXTURE_ENABLED, true);
+        //  this.getIntent().putExtra(CallActivity.EXTRA_VIDEO_CALL, false);
+
+        String streamId = null ;//"stream1";
+        String roomId = "room1";
+        conferenceManager = new ConferenceManager(
+                this,
+                this,
+                getIntent(),
+                MainActivity.SERVER_URL,
+                roomId,
+                publishViewRenderer,
+                playViewRenderers,
+                streamId
+        );
+
+        conferenceManager.setOpenFrontCamera(true);
+    }
+    public void joinConference(View v) {
+        if (!conferenceManager.isJoined()) {
+            ((Button)v).setText("Leave");
+            conferenceManager.joinTheConference();
+        }
+        else {
+            ((Button)v).setText("Join");
+            conferenceManager.leaveFromConference();
+        }
+    }
+
+
+    @Override
+    public void onPlayStarted() {
+        Log.w(getClass().getSimpleName(), "onPlayStarted");
+        Toast.makeText(this, "Play started", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPublishStarted() {
+        Log.w(getClass().getSimpleName(), "onPublishStarted");
+        Toast.makeText(this, "Publish started", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onPublishFinished() {
+        Log.w(getClass().getSimpleName(), "onPublishFinished");
+        Toast.makeText(this, "Publish finished", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onPlayFinished() {
+        Log.w(getClass().getSimpleName(), "onPlayFinished");
+        Toast.makeText(this, "Play finished", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void noStreamExistsToPlay() {
+        Log.w(getClass().getSimpleName(), "noStreamExistsToPlay");
+        Toast.makeText(this, "No stream exist to play", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onError(String description) {
+        Toast.makeText(this, "Error: "  +description , Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onSignalChannelClosed(WebSocket.WebSocketConnectionObserver.WebSocketCloseNotification code) {
+        Toast.makeText(this, "Signal channel closed with code " + code, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onDisconnected() {
+        Log.w(getClass().getSimpleName(), "disconnected");
+        Toast.makeText(this, "Disconnected", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnected() {
+        //it is called when connected to ice
+    }
+
+    @Override
+    public void onTrackList(String[] tracks) {
+
+    }
+
+}
+
