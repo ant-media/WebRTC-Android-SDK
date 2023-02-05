7cba78b14b8b0e0cd8c2f813d9d1dbdef4ced067
burak58 <bk5806>
2023-02-05 23:20:18 +0300

    change Random method

--- webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/PushToTalkActivity.java
+++ webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/PushToTalkActivity.java
@@ -90,7 +90,7 @@ public class PushToTalkActivity extends Activity implements IWebRTCListener, IDa
         this.getIntent().putExtra(EXTRA_CAPTURETOTEXTURE_ENABLED, true);
         //  this.getIntent().putExtra(CallActivity.EXTRA_VIDEO_CALL, false);
 
-        String streamId = "stream"+new Random().nextInt(10000);
+        String streamId = "stream"+(int)(Math.random()*99999);
         String roomId = "room1";
         String serverUrl = "ws://192.168.1.25:5080/WebRTCAppEE/websocket";
         conferenceManager = new MultitrackConferenceManager(
