48beefe4e487c2f6e523b3443f253b6f9d855e49
Last Peony <yguney@sabanciuniv.edu>
2024-05-20 13:57:24 +0700

    Are you speaking  (#66)
    * add sound meter to get local microphone decibel from AudioRecord
    detect audio level even microphone is muted and implement are you speaking indicator
    
    * create separate app
    
    * add activity to manifes
    
    * move sound meter to framework
    
    * add sample app to list
    
    * set codecov token
    
    * change codecov version
    
    * change codecov version
    
    ---------
    
    Co-authored-by: lastpeony <emre.guney@antmedia.io>
    Co-authored-by: burak-58 <burak@antmedia.io>

--- webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/MainActivity.java
+++ webrtc-android-sample-app/src/main/java/io/antmedia/webrtc_android_sample_app/MainActivity.java
@@ -26,6 +26,7 @@ import io.antmedia.webrtc_android_sample_app.advanced.MP3PublishActivity;
 import io.antmedia.webrtc_android_sample_app.advanced.MP4PublishActivity;
 import io.antmedia.webrtc_android_sample_app.advanced.MP4PublishWithSurfaceActivity;
 import io.antmedia.webrtc_android_sample_app.advanced.MultiTrackPlayActivity;
+import io.antmedia.webrtc_android_sample_app.advanced.PublishActivityWithAreYouSpeaking;
 import io.antmedia.webrtc_android_sample_app.advanced.USBCameraActivity;
 import io.antmedia.webrtc_android_sample_app.basic.ConferenceActivity;
 import io.antmedia.webrtc_android_sample_app.basic.DataChannelOnlyActivity;
@@ -62,7 +63,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         addActivity(MP4PublishWithSurfaceActivity.class, "mp4 with Surface");
         addActivity(USBCameraActivity.class, "USB Camera");
         addActivity(MultiTrackPlayActivity.class, "Multi Track");
+        addActivity(PublishActivityWithAreYouSpeaking.class, "Are you talking?");
         addActivity(SettingsActivity.class, "Settings");
+        addActivity(PublishActivityWithAreYouSpeaking.class, "Publish with Are You Speaking");
 
     }
 
