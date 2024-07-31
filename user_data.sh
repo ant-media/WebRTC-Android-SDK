44b763c4879299cbfe4b5ee949911d077c0a7665
Murat Ugur Eminoglu <murat@murat.ws>
2024-07-31 17:00:39 +0300

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -8,27 +8,27 @@ RUNNER_VERSION="2.317.0"
 SDK_VERSION="11076708"
 RUNNER_ORG=""
 RUNNER_TOKEN=$(curl -s -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $GITHUB_TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$RUNNER_ORG/actions/runners/registration-token | jq -r .token)
-
+USER="ubuntu"
 
 # Install Runner
 #useradd -m -d /home/runner -s /bin/bash runner
 #sudo usermod -aG sudo runner
 #echo "runner ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-su - ubuntu -c "
+su - $USER -c "
 mkdir -p actions-runner
 cd actions-runner
 curl -o actions-runner-linux-x64-$RUNNER_VERSION.tar.gz -L https://github.com/actions/runner/releases/download/v$RUNNER_VERSION/actions-runner-linux-x64-$RUNNER_VERSION.tar.gz
 tar xzf ./actions-runner-linux-x64-$RUNNER_VERSION.tar.gz"
 
 su - ubuntu -c "
-/home/ubuntu/actions-runner/config.sh --url https://github.com/$RUNNER_ORG --token $RUNNER_TOKEN --unattended"
+/home/$USER/actions-runner/config.sh --url https://github.com/$RUNNER_ORG --token $RUNNER_TOKEN --unattended"
 
-cd /home/ubuntu/actions-runner/
-./svc.sh install runner
+cd /home/$USER/actions-runner/
+./svc.sh install $USER
 ./svc.sh start
 
 # Install Android SDK
-su - ubuntu -c "
+su - $USER -c "
 wget https://dl.google.com/android/repository/commandlinetools-linux-"$SDK_VERSION"_latest.zip
 mkdir -p ~/android/cmdline-tools/latest
 mkdir -p ~/android/tmp
@@ -38,7 +38,7 @@ echo $HOME >> /tmp/id.txt
 whoami >> /tmp/id.txt
 cat <<EOF >> ~/.bashrc
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64/
-export ANDROID_HOME=/home/ubuntu/android/
+export ANDROID_HOME=/home/$USER/android/
 export PATH=\${ANDROID_HOME}/tools:\${PATH}
 export PATH=\${ANDROID_HOME}/emulator:\${PATH}
 export PATH=\${ANDROID_HOME}/platform-tools:\${PATH}
