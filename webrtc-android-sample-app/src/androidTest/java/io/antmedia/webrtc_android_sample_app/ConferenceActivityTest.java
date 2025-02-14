a2782fe708979339e7d0c3680d7f92a9a2ba95f8
USAMA <usamawizard@gmail.com>
2025-02-14 10:31:51 +0530

    refactor shift UI things on activity side

--- webrtc-android-sample-app/src/androidTest/java/io/antmedia/webrtc_android_sample_app/ConferenceActivityTest.java
+++ webrtc-android-sample-app/src/androidTest/java/io/antmedia/webrtc_android_sample_app/ConferenceActivityTest.java
@@ -465,7 +465,7 @@ public class ConferenceActivityTest {
         onView(withId(R.id.broadcasting_text_view))
                 .check(matches(withText(R.string.live)));
 
-        //Thread.sleep(3000);
+        Thread.sleep(3000);
 
         onView(withId(R.id.join_conference_button)).perform(click());
 
