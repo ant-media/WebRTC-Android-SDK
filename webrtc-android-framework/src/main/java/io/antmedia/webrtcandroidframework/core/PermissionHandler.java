5d5d88713eda3edc1aff92343c5ba2083e616b69
burak <burak@antmedia.io>
2025-04-08 11:06:23 +0300

    update version

--- webrtc-android-framework/src/main/java/io/antmedia/webrtcandroidframework/core/PermissionHandler.java
+++ webrtc-android-framework/src/main/java/io/antmedia/webrtcandroidframework/core/PermissionHandler.java
@@ -6,8 +6,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.util.Log;
 
-import androidx.core.app.ActivityCompat;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -18,18 +16,20 @@ public class PermissionHandler {
     public static final int PUBLISH_PERMISSION_REQUEST_CODE = 1;
     public static final int PLAY_PERMISSION_REQUEST_CODE = 2;
 
-
-    public static final String[] BLUETOOTH_PERMISSIONS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S ?
-            new String[] {Manifest.permission.BLUETOOTH_CONNECT} : new String[] {} ;
+    public static final String[] BLUETOOTH_PERMISSIONS = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+            ? new String[]{Manifest.permission.BLUETOOTH_CONNECT}
+            : new String[]{};
 
     public static final String[] CAMERA_PERMISSIONS =
-            new String[] {Manifest.permission.CAMERA};
+            new String[]{Manifest.permission.CAMERA};
 
     public static final String[] PUBLISH_PERMISSIONS =
-            new String[] {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+            new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
 
-    public static final String[] REQUIRED_MINIMUM_PERMISSIONS = {"android.permission.MODIFY_AUDIO_SETTINGS",
-            "android.permission.INTERNET"};
+    public static final String[] REQUIRED_MINIMUM_PERMISSIONS = {
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+            Manifest.permission.INTERNET
+    };
 
     public static final String[] FULL_PERMISSIONS = concatArrays(
             BLUETOOTH_PERMISSIONS,
@@ -53,78 +53,80 @@ public class PermissionHandler {
         return result;
     }
 
-    public static boolean checkCameraPermissions(Activity activity){
+    public static boolean checkCameraPermissions(Activity activity) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
         permissions.addAll(Arrays.asList(CAMERA_PERMISSIONS));
         return hasPermissions(activity, permissions);
     }
 
-    public static boolean checkPublishPermissions(Activity activity, boolean bluetoothEnabled){
+    public static boolean checkPublishPermissions(Activity activity, boolean bluetoothEnabled) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
         permissions.addAll(Arrays.asList(PUBLISH_PERMISSIONS));
 
-        if(bluetoothEnabled){
+        if (bluetoothEnabled) {
             permissions.addAll(Arrays.asList(BLUETOOTH_PERMISSIONS));
         }
 
         return hasPermissions(activity, permissions);
     }
 
-    public static boolean checkPlayPermissions(Activity activity, boolean bluetoothEnabled){
+    public static boolean checkPlayPermissions(Activity activity, boolean bluetoothEnabled) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
-        if(bluetoothEnabled){
+        if (bluetoothEnabled) {
             permissions.addAll(Arrays.asList(BLUETOOTH_PERMISSIONS));
         }
-        return hasPermissions(activity,permissions);
+        return hasPermissions(activity, permissions);
     }
 
     public static void requestPermissions(Activity activity, List<String> permissions, int requestCode) {
-        String[] permissionArray = new String[permissions.size()];
-        permissions.toArray(permissionArray);
-        ActivityCompat.requestPermissions(activity, permissionArray, requestCode);
+        if (activity == null) return;
+        String[] permissionArray = permissions.toArray(new String[0]);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            activity.requestPermissions(permissionArray, requestCode);
+        }
     }
 
     public static boolean hasPermissions(Activity activity, List<String> permissions) {
         if (activity != null && permissions != null) {
             for (String permission : permissions) {
-                if (ActivityCompat.checkSelfPermission(activity, permission)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    Log.w(PermissionHandler.class.getSimpleName(), "Permission required:"+permission);
-                    return false;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (activity.checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+                        Log.w(PermissionHandler.class.getSimpleName(), "Permission required: " + permission);
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
 
-    public static void requestCameraPermissions(Activity activity){
+    public static void requestCameraPermissions(Activity activity) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
         permissions.addAll(Arrays.asList(CAMERA_PERMISSIONS));
         requestPermissions(activity, permissions, CAMERA_PERMISSION_REQUEST_CODE);
     }
 
-    public static void requestPublishPermissions(Activity activity, boolean bluetoothEnabled){
+    public static void requestPublishPermissions(Activity activity, boolean bluetoothEnabled) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
         permissions.addAll(Arrays.asList(PUBLISH_PERMISSIONS));
 
-        if(bluetoothEnabled){
+        if (bluetoothEnabled) {
             permissions.addAll(Arrays.asList(BLUETOOTH_PERMISSIONS));
         }
         requestPermissions(activity, permissions, PUBLISH_PERMISSION_REQUEST_CODE);
     }
 
-    public static void requestPlayPermissions(Activity activity, boolean bluetoothEnabled){
+    public static void requestPlayPermissions(Activity activity, boolean bluetoothEnabled) {
         ArrayList<String> permissions = new ArrayList<>();
         permissions.addAll(Arrays.asList(REQUIRED_MINIMUM_PERMISSIONS));
-        if(bluetoothEnabled){
+        if (bluetoothEnabled) {
             permissions.addAll(Arrays.asList(BLUETOOTH_PERMISSIONS));
         }
         requestPermissions(activity, permissions, PLAY_PERMISSION_REQUEST_CODE);
     }
 }
-
